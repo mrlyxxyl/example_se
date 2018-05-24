d98cb562718dcddd8f7fcb00bcb4a99fef746746
liyuexin <1290821453@qq.com>
2018-05-24 17:00:44 +0800

    update

--- src/main/java/com/yx/test/fileOper/PdfImage.java
+++ src/main/java/com/yx/test/fileOper/PdfImage.java
@@ -1,7 +1,6 @@
 package com.yx.test.fileOper;
 
 import com.itextpdf.text.Image;
-import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.*;
 
 import java.io.File;
@@ -16,27 +15,31 @@ import java.io.InputStream;
  */
 public class PdfImage {
     public static void main(String[] args) throws Exception {
-        addImage();
+        addImage("e:/test.pdf", "e:/" + System.currentTimeMillis() + ".pdf", "e:/test.png", 1, 10, 10, 100, 50);
     }
 
-    public static void addImage() throws Exception {
-        String templatePath = "e:/test.pdf";//模板文件路径
-        String targetPath = "e:/target.pdf";//生成的文件路径
-        String fieldName = "field";//域名
-        String imagePath = "e:/test.png";//图片路径
-        InputStream input = new FileInputStream(new File(templatePath));
+    /**
+     * pdf中添加图片
+     *
+     * @param srcPath   原文件路径
+     * @param desPath   生成文件路径
+     * @param imagePath 图片路径
+     * @param page      页码
+     * @param x         x坐标
+     * @param y         y坐标
+     * @param w         图片宽
+     * @param h         图片高
+     * @throws Exception
+     */
+    public static void addImage(String srcPath, String desPath, String imagePath, int page, float x, float y, float w, float h) throws Exception {
+        InputStream input = new FileInputStream(new File(srcPath));
         PdfReader reader = new PdfReader(input);
-        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(targetPath));
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(desPath));
         AcroFields form = stamper.getAcroFields();
         form.addSubstitutionFont(BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED));
-        // 通过域名获取所在页和坐标，左下角为起点
-        int pageNo = form.getFieldPositions(fieldName).get(0).page;
-        Rectangle signRect = form.getFieldPositions(fieldName).get(0).position;
-        float x = signRect.getLeft();
-        float y = signRect.getBottom();
-        Image image = Image.getInstance(imagePath);//读图片
-        PdfContentByte under = stamper.getOverContent(pageNo);
-        image.scaleToFit(signRect.getWidth(), signRect.getHeight());
+        Image image = Image.getInstance(imagePath);
+        PdfContentByte under = stamper.getOverContent(page);
+        image.scaleToFit(w, h);
         image.setAbsolutePosition(x, y);
         under.addImage(image);
         stamper.close();
